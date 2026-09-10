005e0f82cffb8ddf359940cfe8daabf0ab22bcb1
omar-santos-dev <omar.santos0420@proton.me>
2026-09-10 12:50:16 -0400

    Update API path to include full URL for creation

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ form.addEventListener("submit", async function(event) {
     */
 
     const apiPath =
-        "/api/2209290/account_provisioning_api";
+        "https://dev411791.service-now.com/api/2209290/account_provisioning_api/create";
 
 
     const payload = {
